782135d2d094e8f356332847bab9a5516f23ecf0
vahan <v_levonyan@yahoo.com>
2018-09-03 18:48:06 +0400

    deleted useless

---
+++ algorithm/union/UF.java
@@ -0,0 +1,19 @@
+package union;
+
+public class UF {
+	
+	public UF(int N) {
+	}
+
+	public void union(int p, int q) {
+	}
+
+	public boolean connected(int p, int q) {
+	}
+
+	private int find(int p){
+	}
+
+	private int count() {
+	}
+}
